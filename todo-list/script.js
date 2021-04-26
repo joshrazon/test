d3b8cd5a93f9df17cdbce5e8a8ee4b6bbc755fdd
Josh <joshrazonn@gmail.com>
2021-04-25 22:51:21 -0600

    rendered pokedata

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,16 +1,22 @@
+
+
 const pokeData = () => {
+  let arrayData = [];
+  let myP = document.querySelectorAll('p');
+
   fetch("https://pokeapi.co/api/v2/pokemon/ditto")
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+
+      arrayData.push(data.abilities)
+
+      console.log(arrayData[0][0].ability.name)
+
+      for (let i = 0; i < myP.length; i++) {
+        myP[i].innerText = arrayData[0][i].ability.name;
+      }
+    })
 }
 
 pokeData()
 
-// let myP = document.querySelector('p');
-// console.log(myP)
-
-// function changeElem() {
-
-// }
-
-// changeElem();
